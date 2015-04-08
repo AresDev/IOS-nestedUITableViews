3416fc015035c00e7b5e1b3f250e95d42e669b25
Armando Restrepo <AresDev@Armandos-Mac-mini.local>
2015-04-08 08:12:19 -0500

    Add files

---
+++ NestedTableViews/ParentTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ParentTableViewController.h
+//  NestedTableViews
+//
+//  Created by Armando Restrepo on 4/7/15.
+//  Copyright (c) 2015 AresDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ParentTableViewController : UITableViewController
+
+@end
